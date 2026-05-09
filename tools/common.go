0c793ae198e567cddab48d9bb971a71076ffa3d3
Adam Maly <amaly@redhat.com>
2026-05-09 22:15:46 +0200

    fix CI

--- tools/common.go
+++ tools/common.go
@@ -1,6 +1,7 @@
 package tools
 
 import (
+	"context"
 	"fmt"
 
 	core "github.com/amaly/mcp-server-rhoai/core"
@@ -8,9 +9,17 @@ import (
 	"k8s.io/client-go/kubernetes"
 )
 
+const defaultPVCSize = "10Gi"
+
 var GetClientSet = func() (kubernetes.Interface, error) { return core.LogIntoClusterClientSet() }
 
 var GetDynamicClient = func() (dynamic.Interface, error) { return core.LogIntoClusterDynamic() }
+
+var getUptimeFromWorkbenchFn = getUptimeFromWorkbench
+var getDiskUsageFromPVCFn = getDiskUsageFromPVC
+var getImageInfoFn = func(ctx context.Context, displayName, version string) (string, string, string, error) {
+	return GetImageInfo(ctx, displayName, version)
+}
 
 func convertToString(val interface{}) string {
 	switch v := val.(type) {
